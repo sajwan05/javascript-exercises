203f2636b6db712fca56e5700cf3cb6de9058ea3
sajwan05 <surisajwan1998@gmail.com>
2025-09-07 17:06:20 +0000

    Solved sum all excercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,43 @@
-const sumAll = function() {
+// const sumAll = function(start, end) {
+//     let sum = 0;
+//     if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+//         return "ERROR";
+//     } 
+//     else if (start > end) {
+//         for (let j = end; j <= start; j++) {
+//             sum += j;
+//         }
+//     }else{
+//          for(let i = start; i <= end; i++) {
+//         sum += i;
+//     }
+//     }
+   
 
-};
+//     return sum;
+// };
+
+
+// Alternative Approach:
+
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return `ERROR`;   
+    if (min < 0 || max < 0) return `ERROR`;
+
+    if (min > max) {
+        let t = min;
+        min = max;
+        max = t;
+    }
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
