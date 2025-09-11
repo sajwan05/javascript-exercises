49ade30f0b326b26311e75dfbba8c881a50a5885
sajwan05 <surisajwan1998@gmail.com>
2025-09-11 07:33:46 +0000

    solved few more excercises

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if (year % 4 === 0 && !(year % 100 === 0) ){
+    //     return true;
+    // }else if (year % 100 === 0 && year % 400 === 0 ){
+    //     return true;
+    // }else {
+    //     return false;
+    // }
 
-};
+    let divisibleByFour = year % 4 === 0;
+    let divisibleByHundred = year % 100 === 0;
+    let divisibleByFourHundred = year % 400 === 0;
+
+    if (divisibleByFour && !divisibleByHundred || divisibleByFourHundred){
+        return true;
+    }else {
+        return false;
+    }
+ }
 
 // Do not edit below this line
 module.exports = leapYears;
