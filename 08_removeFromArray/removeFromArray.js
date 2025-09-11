506492abab621bc3a12582411690a098ef60e274
sajwan05 <surisajwan1998@gmail.com>
2025-09-11 09:03:51 +0000

    Solved all excercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argu) {
+//   12345, 3 1245
+    return array.filter((item) => !argu.includes(item));
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = Math.round(((temp - 32) * (5/9)) * 10) / 10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = Math.round((temp *  9/5 + 32) * 10) /10; 
+  return temp; 
 };
 
 // Do not edit below this line
